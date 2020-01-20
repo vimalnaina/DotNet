fa68d11eab215cfc397fb1f273921444fd31a1d9
vimal naina <nainavimal2000@gmail.com>
2020-01-20 13:10:39 +0530

    practical3

---
+++ DotNetPracticals/Practical_3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Practical_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String name, country;
+
+            Console.Write("Enter Your Name: ");
+            name = Console.ReadLine();
+            Console.Write("Enter Your Country: ");
+            country = Console.ReadLine();
+            Console.WriteLine("Hi, " + name + " from country " + country);
+            Console.Read();
+        }
+    }
+}
